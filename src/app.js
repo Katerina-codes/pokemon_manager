aaeaea1a17d724e824bb0fbe69f5ea8f47f812c5
Katerina Georgiou <katerinaldg@gmail.com>
2018-05-12 10:50:54 +0100

    Add search box, button, image and styling.

--- src/app.js
+++ src/app.js
@@ -4,8 +4,14 @@ import ReactDOM from 'react-dom'
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className='home'>
         <h1>Pokemon times!</h1>
+
+        <img src='images/rapidash.png' className='pokemon' alt='A horse type Pokemon named Rapidash'/>
+
+        <input className='search-box' type='text' placeholder='Enter the name of a Pokemon...'/>
+
+        <button type className='button'>Search NOW!</button>
       </div>
     )
   }
